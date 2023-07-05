dfd066d19652ad2cb8d8ebb1b633fe15a419c904
Loloppe <LaurentBlaisCote@outlook.com>
2023-07-05 19:05:41 -0400

    .

--- ChroMapper-LightModding/Configs/Configs.cs
+++ ChroMapper-LightModding/Configs/Configs.cs
@@ -14,7 +14,7 @@
         public double ShortWallTrailDuration { get; set; } = 0.25;
         public double MaximumDodgeWallPerSecond { get; set; } = 3.5;
         public double SubjectiveDodgeWallPerSecond { get; set; } = 2;
-        public int MaxChainRotation { get; set; } = 30;
+        public double MaxChainRotation { get; set; } = 30;
         public double MaxChainBeatLength { get; set; } = 0.45;
         public double ChainLinkVsAir { get; set; } = 0.75;
         public double VBMaxBottomNoteTime { get; set; } = 0.075;
@@ -24,7 +24,7 @@
         public double VBMaxBombTime { get; set; } = 0.15;
         public double VBMinBombTime { get; set; } = 0.20;
         public double VBMinimum { get; set; } = 0.025;
-        public int ParityWarningAngle { get; set; } = 180;
+        public double ParityWarningAngle { get; set; } = 180;
         public bool ParityDebug { get; set; } = false;
     }
 }
