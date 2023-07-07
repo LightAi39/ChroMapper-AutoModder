02032ee527492a12afde70de52156a11cbabbd2f
LightAi39 <infowesselvanhemert@gmail.com>
2023-07-07 03:19:47 +0200

    version bump & AssemblyInfo update

--- ChroMapper-LightModding/Properties/AssemblyInfo.cs
+++ ChroMapper-LightModding/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChroMapper-LightModding")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ChroMapper-AutoModder")]
+[assembly: AssemblyDescription("A Plugin for ChroMapper that assists mappers and modders by implementing auto-checking features as well as comments and replies, with a seamless in-editor experience.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChroMapper-LightModding")]
+[assembly: AssemblyProduct("ChroMapper-AutoModder")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.3.0")]
-[assembly: AssemblyFileVersion("0.6.3.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
