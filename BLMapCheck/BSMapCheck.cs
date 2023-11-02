3f17dbedcdde85bfa2144a7ad6dc73650275d19a
LightAi39 <infowesselvanhemert@gmail.com>
2023-11-02 19:35:34 +0100

    added main cs file

---
+++ BLMapCheck/BSMapCheck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLMapCheck
+{
+    public class BSMapCheck
+    {
+        // this should be the entry point for the program
+    }
+}
