8a7cdba9689c2419c4f4c021c20668caf32491b9
Loloppe <LaurentBlaisCote@outlook.com>
2023-11-03 23:42:09 -0400

    V2 to V3 conversion

--- BLMapCheck/BeatmapScanner/CriteriaCheck/CriteriaCheckManager.cs
+++ BLMapCheck/BeatmapScanner/CriteriaCheck/CriteriaCheckManager.cs
@@ -11,7 +11,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 using DifficultyV3 = BLMapCheck.Classes.MapVersion.Difficulty.DifficultyV3;
 using Light = BLMapCheck.BeatmapScanner.CriteriaCheck.Difficulty.Light;
 using Parity = BLMapCheck.BeatmapScanner.CriteriaCheck.Difficulty.Parity;
